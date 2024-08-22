d5b3dcd87d98991b9a48cf3589f78c5f821b5ef7
hayoonjeon <dbs9830@gmail.com>
2024-08-22 09:49:28 +0900

    asd

--- src/com/ict/day16/Ex09.java
+++ src/com/ict/day16/Ex09.java
@@ -1,7 +1,24 @@
 package com.ict.day16;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Ex09 {
 	public static void main(String[] args) {
-		// 로또 생성기 (5줄, 보너스번호까지)
+		for (int i = 0; i < 5; i++) {
+			Set<Integer> lottoNumbers = new HashSet<Integer>();
+			while (lottoNumbers.size()<6) {
+				int number = (int)(Math.random()*45)+1;
+				lottoNumbers.add(number);
+			}
+				
+				int bonusNumbers;
+				do {
+					bonusNumbers = (int)(Math.random()*45)+1;
+				} while (lottoNumbers.contains(bonusNumbers));
+				System.out.println(lottoNumbers+","+bonusNumbers);
+				
+			}
+		}
 	}
-}
+
